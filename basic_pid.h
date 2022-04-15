a8d19d7c4c53bda7cac54c0aa660f08fb42f49df
Matt <mattheww183@gmail.com>
2022-04-15 10:53:43 +0100

    Fixed header file

--- basic_pid.h
+++ basic_pid.h
@@ -14,3 +14,16 @@ struct PID {
     float prev_measured;
     absolute_time_t time_prev;
 };
+
+
+// PID coefficents (example implementation):
+// Kp: Proportial term (0.5)
+// Ki: Integral term (0.05)
+// Kd: Derivative term (0.01)
+// Tau: Used to smooth setpoint change (0.02)
+// Windup limit: Limits integral value to +/- (1)
+void PID_Coefficents(struct PID *pid_struct, float Kp, float Ki, float Kd, float tau, float intergrator_lmt);
+
+
+// Computes PID, outputs raw PID value around 0 point.
+float PID_Compute(absolute_time_t time, struct PID *pid);
